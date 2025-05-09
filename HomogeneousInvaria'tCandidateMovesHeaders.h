9474cba93d621137d62351b523b425092afb6df7
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-09 12:13:08 +0200

    Create HomogeneousInvaria'tCandidateMovesHeaders.h

---
+++ HomogeneousInvaria'tCandidateMovesHeaders.h
@@ -0,0 +1,19 @@
+//may be slow. this is due to the
+//lack of models inherited from this repository.
+//but we can have 10^15 seconds (mpi prepa).
+//then we will fully use many models tonreduce the timelzpse
+//between two moves. notice that im not using 3 moves.
+//then we have comolexity log(10^15).
+
+class homogeneousTimeMatcher
+{
+public:
+
+ //first use an intherited pattern for auto play
+
+
+  //then do alpha beta
+
+ //then use impulse theories to have differential esuations.
+
+}
